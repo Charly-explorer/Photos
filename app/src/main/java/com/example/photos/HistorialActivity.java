27a74e04dd09ab89f0dd4ee7ccd017ed64dcff2f
johelmejias <jomejiasma@est.utn.ac.cr>
2024-11-18 14:14:39 -0600

    Se crea la pantallas de perfil e historial de configuraciones

--- app/src/main/java/com/example/photos/HistorialActivity.java
+++ app/src/main/java/com/example/photos/HistorialActivity.java
@@ -11,40 +11,36 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class ConfigurationActivity extends AppCompatActivity {
-
-    Button btnchat2, btncamera, btnconfiguraciones;
-
+public class HistorialActivity extends AppCompatActivity {
+    Button btnBack2,btnConfig2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_configuration);
+        setContentView(R.layout.activity_historial);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        btnchat2 = (Button) findViewById(R.id.btnChat2);
-        btncamera = (Button) findViewById(R.id.btnCamara2);
-        btnconfiguraciones=(Button) findViewById(R.id.btnConfiguracion2);
+        btnBack2 = (Button) findViewById(R.id.btnBackHis);
+        btnConfig2 = (Button) findViewById(R.id.btnConfHis);
 
-        btncamera.setOnClickListener(new View.OnClickListener() {
+        btnBack2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent CameraActivity = new Intent(ConfigurationActivity.this, CameraActivity.class );
-                startActivity(CameraActivity);
+                Intent ConfigActivity = new Intent(HistorialActivity.this, ConfigurationActivity.class );
+                startActivity(ConfigActivity);
             }
         });
 
-        btnchat2.setOnClickListener(new View.OnClickListener() {
+        btnConfig2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent ChatActivity = new Intent(ConfigurationActivity.this, ChatActivity.class);
-                startActivity(ChatActivity);
+                Intent ConfigActivity = new Intent(HistorialActivity.this, ConfigurationActivity.class);
+                startActivity(ConfigActivity);
             }
         });
-
     }
 }
--- app/src/main/java/com/example/photos/PerfilActivity.java
+++ app/src/main/java/com/example/photos/PerfilActivity.java
@@ -11,40 +11,38 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class ConfigurationActivity extends AppCompatActivity {
-
-    Button btnchat2, btncamera, btnconfiguraciones;
+public class PerfilActivity extends AppCompatActivity {
+    Button btnBack,btnConfig;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_configuration);
+        setContentView(R.layout.activity_perfil);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        btnchat2 = (Button) findViewById(R.id.btnChat2);
-        btncamera = (Button) findViewById(R.id.btnCamara2);
-        btnconfiguraciones=(Button) findViewById(R.id.btnConfiguracion2);
+        btnBack = (Button) findViewById(R.id.btnBack);
+        btnConfig = (Button) findViewById(R.id.btnConf);
 
-        btncamera.setOnClickListener(new View.OnClickListener() {
+        btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent CameraActivity = new Intent(ConfigurationActivity.this, CameraActivity.class );
-                startActivity(CameraActivity);
+                Intent ConfigActivity = new Intent(PerfilActivity.this, ConfigurationActivity.class );
+                startActivity(ConfigActivity);
             }
         });
 
-        btnchat2.setOnClickListener(new View.OnClickListener() {
+        btnConfig.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent ChatActivity = new Intent(ConfigurationActivity.this, ChatActivity.class);
-                startActivity(ChatActivity);
+                Intent ConfigActivity = new Intent(PerfilActivity.this, ConfigurationActivity.class);
+                startActivity(ConfigActivity);
             }
         });
-
     }
+
 }
