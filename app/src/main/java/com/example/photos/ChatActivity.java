56e1db548bb5441af4fe9b09869116bb6573aaf5
johelmejias <jomejiasma@est.utn.ac.cr>
2024-11-17 19:10:12 -0600

    Se crea la pantalla del chat

--- app/src/main/java/com/example/photos/ChatActivity.java
+++ app/src/main/java/com/example/photos/ChatActivity.java
@@ -1,6 +1,9 @@
 package com.example.photos;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class ChatActivity extends AppCompatActivity {
+    Button btnchat, btncamera, btnconfiguraciones;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +25,24 @@ public class ChatActivity extends AppCompatActivity {
             return insets;
         });
 
+        btnchat = (Button) findViewById(R.id.btnChat);
+        btncamera = (Button) findViewById(R.id.btnCamara);
+        btnconfiguraciones=(Button) findViewById(R.id.btnConfiguracion);
+
+        btncamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent CameraActivity = new Intent(ChatActivity.this, CameraActivity.class );
+                startActivity(CameraActivity);
+            }
+        });
+        btnconfiguraciones.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ConfigurationActivity = new Intent(ChatActivity.this, ConfigurationActivity.class);
+                startActivity(ConfigurationActivity);
+            }
+        });
 
     }
 }
